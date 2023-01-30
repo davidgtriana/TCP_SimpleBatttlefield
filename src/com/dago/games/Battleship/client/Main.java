a16aaee3be168ffcc8d763bc5c356a1121857fdf
David Triana <hello@davidgtriana.com>
2023-01-30 11:40:21 -0400

    Documentation in Client Main Class

--- src/com/dago/games/Battleship/client/Main.java
+++ src/com/dago/games/Battleship/client/Main.java
@@ -1,3 +1,12 @@
+/**
+ * Main class for the Battleship client.
+ * This class creates a JFrame and sets its default close operation and bounds.
+ * The main panel is set with a null layout and added as the content pane.
+ * The window adapter is used to dispose the game frame when the window is closing.
+ *
+ * @author Dago
+ * @version 1.0
+ */
 package com.dago.games.Battleship.client;
 
 import com.dago.GameFrame;
@@ -7,11 +16,17 @@ import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-public class Main extends JFrame{																							// Create the general class that inherits methods and atrributes of the class JFrame
+public class Main extends JFrame{
     public static final long serialVersionUID = 1L;
     public static JPanel mainPanel = new JPanel();
     public static GameFrame app = new GameFrame();
 
+    /**
+     * The main method for the Battleship client.
+     * It creates a new Main instance and sets it to be visible.
+     *
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
             public void run() {
@@ -25,6 +40,10 @@ public class Main extends JFrame{																							// Create the general cl
         });
     }
 
+    /**
+     * The constructor for the Main class.
+     * It sets the default close operation, bounds, layout for the main panel, and adds the window adapter.
+     */
     Main(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setBounds(0, 0 , 1280, 720);
@@ -35,6 +54,4 @@ public class Main extends JFrame{																							// Create the general cl
                 app.dispose();
             }});
     }
-
-
 }
